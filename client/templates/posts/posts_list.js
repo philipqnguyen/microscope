3ea7a38ae7d9afb0c765f8be31efc09db9ae5041
Philip Q Nguyen <supertaru@gmail.com>
2015-08-29 12:09:37 -0700

    Add postsList and posts

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,17 @@
+var postsData = [
+  {
+    title: 'Indroducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
